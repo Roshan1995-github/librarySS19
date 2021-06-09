0410f281f6a998589be28067513e9418777fbf9b
Roshan Manohar <roshanmanohar.10v@gmail.com>
2021-06-09 11:05:01 +0800

    first commit

---
+++ functions.js
@@ -0,0 +1,109 @@
+function getProperDate(date){
+	var d = new Date(date);
+  	var day = d.getDay();
+  	switch(day){
+  		case 1: 
+  			day = 'Monday';
+  			break;
+  		case 2:
+  			day = 'Tuesday';
+  			break;
+  		case 3:
+  			day = 'Wednesday';
+  			break;
+  		case 4:
+  			day = 'Thursday';
+  			break;
+  		case 5:
+  			day = 'Friday';
+  			break;
+  		case 6:
+  			day = 'Saturday';
+  			break;
+  		case 0:
+  			day = 'Sunday';
+  	}
+  	var date = d.getDate()
+  	var month = d.getMonth()
+    month = getProperMonth(month)  	
+  	var year = d.getFullYear()
+
+  	return [day, (date+"-"+month+"-"+year)];
+}
+
+function getProperMonth(month){
+  while(month > 11){
+    month = month -12
+  }
+  switch(month){
+    case 0:
+      month = 'January';
+      break;
+    case 1: 
+      month = 'February';
+      break;
+    case 2:
+      month = 'March';
+      break;
+    case 3:
+      month = 'April';
+      break;
+    case 4:
+      month = 'May';
+      break;
+    case 5:
+      month = 'June';
+      break;
+    case 6:
+      month = 'July';
+      break;
+    case 7: 
+      month = 'August';
+      break;
+    case 8:
+      month = 'September';
+      break;
+    case 9:
+      month = 'October';
+      break;
+    case 10:
+      month = 'November';
+      break;
+    case 11:
+      month = 'December';
+      break;
+  }
+  return month;
+}
+
+function addDaysToDate(date, addedDays){
+  var d = new Date(date)
+  d.setDate(d.getDate() + addedDays);
+  return d;
+}
+
+function differenceInDates(date1, date2){
+	var firstDate = new Date(date1)
+	var secondDate = new Date(date2)
+	var timeDifference  = firstDate.getTime() - secondDate.getTime()
+	var daysDifference = timeDifference / (1000 * 3600 * 24)
+	return daysDifference;
+}
+
+function capitalize(string){
+ 	var splitStr = string.split(" ")
+ 	var capitalzeStr = ""
+ 	for (var i=0; i<splitStr.length; i++){
+ 		var wordSplit = splitStr[i].split("")
+ 		for (var j=0; j<wordSplit.length; j++){
+ 			if(j==0){
+ 				capitalzeStr = capitalzeStr + wordSplit[j].toUpperCase()
+ 			}else{
+ 				capitalzeStr = capitalzeStr + wordSplit[j].toLowerCase()
+ 			}
+ 		}
+ 		capitalzeStr = capitalzeStr + " "
+ 	}
+
+ 	return capitalzeStr;
+}
